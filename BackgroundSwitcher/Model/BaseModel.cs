6f4aa09910e0335c334dc1b55cc14f2efbed64ba
Michael Kotzjan <mkotzjan@users.noreply.github.com>
2016-06-30 09:23:59 +0200

    Implemented basic MVVM pattern

---
+++ BackgroundSwitcher/Model/BaseModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackgroundSwitcher.Model
+{
+    class BaseModel : INotifyPropertyChanged
+    {
+        public void RaisePropertyChanged(string prop)
+        {
+            if (this.PropertyChanged != null) { this.PropertyChanged(this, new PropertyChangedEventArgs(prop)); }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ BackgroundSwitcher/ViewModel/BaseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackgroundSwitcher.ViewModel
+{
+    class BaseViewModel : INotifyPropertyChanged
+    {
+        public void RaisePropertyChanged(string prop)
+        {
+            if (this.PropertyChanged != null) { this.PropertyChanged(this, new PropertyChangedEventArgs(prop)); }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
